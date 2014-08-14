e8e354d455d703f68db5aab84380b69c48a97dd9
Yuji Kiriki <yujikiriki@gmail.com>
2014-08-14 12:00:08 -0500

    Crea servicio de usuarios

---
+++ backend/app/controllers/Users.scala
@@ -0,0 +1,93 @@
+package controllers
+
+import models.{User, UserJsonFormat}
+import org.slf4j.LoggerFactory
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+import play.api.mvc._
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.api.Cursor
+import reactivemongo.bson.BSONObjectID
+import scala.concurrent.Future
+import play.modules.reactivemongo.json.BSONFormats._
+
+class Users extends Controller with MongoController {
+
+  import Users._
+  import UserJsonFormat._
+
+  def collection: JSONCollection = db.collection[ JSONCollection ]( "users" )
+
+  def get( id: String ) = Action.async {
+    val cursor: Cursor[ User ] = collection.find( Json.obj( "_id" -> BSONObjectID( id ) ) ).sort( Json.obj( "created" -> -1 ) ).cursor[ User ]
+    cursor.collect[ List ]( ).map {
+      users =>
+        if ( users.isEmpty )
+          NotFound
+        else {
+          Ok( Json.toJson( users.head ) )
+        }
+    }
+  }
+
+  def getByRole( role: String ) = Action.async {
+    val cursor: Cursor[ User ] = collection.find( Json.obj( "role" -> role ) ).sort( Json.obj( "created" -> -1 ) ).cursor[ User ]
+    cursor.collect[ List ]( ).map {
+      users =>
+        if ( users.isEmpty )
+          NoContent
+        else {
+          Ok( Json.toJson( users ) )
+        }
+    }
+  }
+
+  def create = Action.async( parse.json ) {
+    request =>
+      request.body.validate[ User ].map {
+        user =>
+          collection.insert( user ).map {
+            lastError =>
+              logger.debug( s"Successfully inserted with LastError: $lastError" )
+              Created( s"User created" )
+          }
+      }.getOrElse( Future.successful( BadRequest( "invalid json" ) ) )
+  }
+
+  def list = Action.async {
+    val cursor: Cursor[ User ] = collection.find( Json.obj( ) ).sort( Json.obj( "created" -> -1 ) ).cursor[ User ]
+    val futureUsersList: Future[ List[ User ] ] = cursor.collect[ List ]( )
+    val futureUsersJsonArray: Future[ JsArray ] = futureUsersList.map {
+      accounts =>
+        Json.arr( accounts )
+    }
+
+    futureUsersJsonArray.map {
+      accounts =>
+        Ok( accounts( 0 ) )
+    }
+  }
+
+  def delete( id: String ) = Action.async {
+    logger.debug( s"User delete begins ${id}" )
+    val cursor: Cursor[ User ] = collection.find( Json.obj( "_id" -> BSONObjectID( id ) ) ).sort( Json.obj( "created" -> -1 ) ).cursor[ User ]
+    cursor.collect[ List ]( ).map {
+      users =>
+        if ( users.isEmpty ) {
+          logger.debug( s"User with id ${id} not found" )
+          NoContent
+        }
+        else {
+          logger.debug( s"The user to be removed: ${id}" )
+          collection.remove( users.head )
+          NoContent
+        }
+    }
+  }
+
+}
+
+object Users {
+  val logger = LoggerFactory.getLogger( classOf[ Users ] )
+}
--- backend/app/models/opportunity.scala
+++ backend/app/models/opportunity.scala
@@ -4,7 +4,7 @@ import play.api.libs.json._
 import reactivemongo.bson._
 import play.modules.reactivemongo.json.BSONFormats._
 
-case class Opportunity(_id: Option[BSONObjectID], name: String, accountId: String, productId: String, quantity: Int, value: Double, state: String, order: OrderIncome, sale: Sale )
+case class Opportunity(_id: Option[BSONObjectID], userId: String, accountId: String, productId: String, name: String, quantity: Int, value: Double, state: String, order: OrderIncome, sale: Sale )
 case class OrderIncome( date: String, month: String, year: String, probability: Int )
 case class Sale( date: String, month: String, year: String, probability: Int )
 
